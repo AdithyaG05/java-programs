b85a1c41ee3f18e804ade7def01372f9163ded08
AdithyaG05 <adithyarawath@gmail.com>
2026-04-21 21:10:57 +0530

    Add files via upload

--- CookieServlet.java
+++ CookieServlet.java
@@ -1,8 +1,4 @@
-// 8b. Build a servlet program to create a cookie to get your name through text box and press submit button(
-// through HTML) to display the message by greeting Welcome back your name ! , you have visited this page
-// n times ( n = number of your visit ) along with the list of cookies and its setvalues and demonstrate the
-// expiry of cookie also.
-package lab8b;
+package lab8;
 
 import java.io.*;
 import javax.servlet.*;
@@ -20,7 +16,7 @@ public class CookieServlet extends HttpServlet {
 
         String name = request.getParameter("username");
 
-        Cookie userCookie = null;
+        Cookie nameCookie = null;
         Cookie countCookie = null;
 
         int count = 0;
@@ -31,7 +27,7 @@ public class CookieServlet extends HttpServlet {
         if (cookies != null) {
             for (Cookie c : cookies) {
                 if (c.getName().equals("user")) {
-                    userCookie = c;
+                    nameCookie = c;
                 }
                 if (c.getName().equals("count")) {
                     countCookie = c;
@@ -39,56 +35,53 @@ public class CookieServlet extends HttpServlet {
             }
         }
 
-        // 🆕 First time user
+        // 🆕 First time login
         if (name != null && !name.isEmpty()) {
-            userCookie = new Cookie("user", name);
+            nameCookie = new Cookie("user", name);
             count = 1;
             countCookie = new Cookie("count", String.valueOf(count));
         }
         // 🔁 Returning user
-        else if (userCookie != null && countCookie != null) {
+        else if (nameCookie != null && countCookie != null) {
             count = Integer.parseInt(countCookie.getValue()) + 1;
             countCookie.setValue(String.valueOf(count));
         }
 
-        // ⏱ Expiry (30 seconds demo)
-        if (userCookie != null) userCookie.setMaxAge(30);
+        // ⏱ Set expiry (30 seconds demo)
+        if (nameCookie != null) nameCookie.setMaxAge(30);
         if (countCookie != null) countCookie.setMaxAge(30);
 
-        if (userCookie != null) response.addCookie(userCookie);
+        // ➕ Add cookies
+        if (nameCookie != null) response.addCookie(nameCookie);
         if (countCookie != null) response.addCookie(countCookie);
 
         // 🎨 OUTPUT
         out.println("<html><body>");
 
-        if (userCookie == null) {
-            out.println("<h2 style='color:red'>Welcome Guest! Please login first</h2>");
+        if (nameCookie == null) {
+            out.println("<h2 style='color:red'>Welcome Guest! Please enter your name</h2>");
             out.println("<form action='CookieServlet' method='get'>");
             out.println("Enter your name: <input type='text' name='username'>");
             out.println("<input type='submit' value='Submit'>");
             out.println("</form>");
         } else {
-            out.println("<h1 style='color:blue'>Welcome back, " + userCookie.getValue() + "!</h1>");
+            out.println("<h1 style='color:blue'>Welcome back, " + nameCookie.getValue() + "!</h1>");
             out.println("<h2 style='color:magenta'>You have visited this page " + count + " times</h2>");
         }
 
-        // 📋 Detailed Cookie List
-        out.println("<h3>Cookie Details:</h3>");
+        // 📋 Display all cookies
+        out.println("<h3>List of Cookies:</h3>");
 
         if (cookies != null) {
             for (Cookie c : cookies) {
-                out.println("<p>");
-                out.println("Name: " + c.getName() + "<br>");
-                out.println("Value: " + c.getValue() + "<br>");
-                out.println("Max Age: " + c.getMaxAge() + " seconds<br>");
-                out.println("------------------------");
-                out.println("</p>");
+                out.println("<p>Name: " + c.getName() + " | Value: " + c.getValue() + "</p>");
             }
         } else {
             out.println("<p>No cookies found</p>");
         }
 
         out.println("<br><a href='index.html'>Go Back</a>");
+
         out.println("</body></html>");
     }
 }
